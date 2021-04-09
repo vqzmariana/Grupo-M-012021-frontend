eccd7fc23bf1c48a073c05a8d85a7221290501ca
vqzmariana <mariaanavqz@gmail.com>
2021-04-09 20:38:24 -0300

    Initial commit - proyecto angular

---
+++ src/test/java/areduunq/desappgrupoM/frontenddesapp/FrontendDesappApplicationTests.java
@@ -0,0 +1,13 @@
+package areduunq.desappgrupoM.frontenddesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendDesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
